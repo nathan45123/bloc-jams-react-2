661a7f82759e220f3147aa1febdc7faf691f1376
unknown <n.navarijo@gmail.com>
2019-03-16 14:27:22 -0500

    MouseEnter

--- bloc/bloc-jams-react/src/components/Album.js
+++ bloc/bloc-jams-react/src/components/Album.js
@@ -13,10 +13,6 @@ class Album extends Component {
       album: album,
       currentSong: album.songs[0],
       isPlaying: false,
-      isPlaying: false,
-      isHovered: false,
-      dynamicClass: 'song-number',
-      targetId: 0
 
     };
 
@@ -26,13 +22,21 @@ class Album extends Component {
 
   play() {
     this.audioElement.play();
-    this.setState({ isPlaying: true });
+    this.setState({
+      isPlaying: true,
+      dynamicClass: 'icon-ion-ios-pause'
+    });
   }
+
 
   pause() {
     this.audioElement.pause();
-    this.setState({ isPlaying: false });
-}
+    this.setState({
+      isPlaying: false,
+      dynamicClass: 'icon ion-ios-play'
+    });
+  }
+
 
   setSong(song) {
     this.audioElement.src = song.audioSrc;
@@ -44,84 +48,84 @@ class Album extends Component {
     if (this.state.isPlaying && isSameSong) {
       this.pause();
     } else {
-      if (!isSameSong) {this.setSong(song); }
+      if (!isSameSong) { this.setSong(song) }
       this.play();
     }
   }
 
-  onMouseEnter(e) {
+
+
+
+
+  mouseEnter(e) {
     console.log(e.target.id);
     if (e.target !== this.state.currentSong && !this.state.isPlaying) {
       this.setState({
         dynamicClass: 'icon ion-ios-play',
         targetId: e.target.id
-      });;
+      });
     }
   }
 
- onMouseLeave(e) {
-   console.log(e.target);
-   if (!this.state.currentSong || !this.state.isPlaying) {
-     this.setState({
-       dynamicClass: 'song-number',
-       targetId: e.target.id
-     });
-   }
-   else if (this.state.currentSong) {
-     this.setState({
-       dynamicClass: 'icon ion-ios-pause',
-       targetId: e.target.id
-     })
-   }
- }
+
+mouseLeave(e) {
+  console.log(e.target.id);
+  if (!this.state.currentSong || !this.state.isPlaying) {
+    this.setState({
+      dynamicClass: 'song-number',
+      targetId: e.target.id
+    });
+  }
+  else if (this.state.currentSong) {
+    this.setState({
+      dynamicClass: 'icon ion-ios-pause',
+      targetId: e.target.id
+    });
+  }
+}
 
 
 
 
 
   render() {
-  return(
-    <section className="album">
-  <section id="album-info">
-    <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-    <div className="album-details">
-    <h1 id="album-title">{this.state.album.title}</h1>
-    <h2 className="artist">{this.state.album.artist}</h2>
-    <div id="release-info">{this.state.album.releaseInfo}</div>
-      </div>
-      </section>
-      <div className="song-list-container">
-      <table id="song-list">
-       <colgroup>
-        <col id="song-number-column" />
-        <col id="song-title-column" />
-        <col id="song-duration-column" />
-        </colgroup>
-        <tbody>
-        {this.state.album.songs.map( (song, index) =>
-          <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-          <td className="song-actions">
-           <button>
-            <span className={(this.state.isPlaying && (this.state.currentSong === song)) ?'hidden-number' : 'song-number'}>{index + 1}</span>
-            <span className={(this.state.isPlaying && (this.state.currentSong === song)) ? 'icon ion-ios-pause' : ''}></span>
-            <span className={(this.state.isPlaying && (this.state.currentSong === song)) ? '' : 'icon ion-ios-play'}></span>
-            </button>
-            </td>
-          <td className="song-number">{song.number}</td>
-          <td className="song-title">{song.title}</td>
-          <td className="song-duration">{song.duration}</td>
-          </tr>
+    return (
+      <section className="album">
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+          <div className="album-details">
+            <h5>ALBUM</h5>
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h5>ARTIST</h5>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+            {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                <td className="song-actions">
+                  <button>
+                    <span className={(this.state.isPlaying && (this.state.currentSong === song)) ?'hidden-number' : 'song-number'}>{index + 1}</span>
+                    <span className={(this.state.isPlaying && (this.state.currentSong === song)) ? 'icon ion-ios-pause' : ''}></span>
+                    <span className={(this.state.isPlaying && (this.state.currentSong === song)) ? '' : 'icon ion-ios-play'}></span>
+                  </button>
+                </td>
+                <td className="song-title">{song.title}</td>
 
-
-
-    )}
-        </tbody>
+              </tr>
+            )}
+          </tbody>
         </table>
-        </div>
-     </section>
-
-  );
-  }
+  </section>
+   );
+ }
 }
 
 export default Album;

4daee645acca3b1ef3e780b0787de7a3c8992d3f
unknown <n.navarijo@gmail.com>
2019-03-17 09:52:25 -0500

    NextCLick

--- bloc/bloc-jams-react/src/components/Album.js
+++ bloc/bloc-jams-react/src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class Album extends Component {
       album: album,
       currentSong: album.songs[0],
       isPlaying: false,
+      duration: album.songs[0].duration
 
     };
 
@@ -53,8 +55,23 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick() {
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+     const newIndex = Math.max(0, currentIndex - 1);
+     const newSong = this.state.album.songs[newIndex];
+     this.setSong(newSong);
+     this.play();
+
+  }
 
 
+ handleNextClick() {
+   const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+   const newIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
+   const newSong = this.state.album.songs[newIndex];
+   this.setSong(newSong);
+   this.play();
+  }
 
 
   mouseEnter(e) {
@@ -88,25 +105,23 @@ mouseLeave(e) {
 
 
 
-  render() {
-    return (
-      <section className="album">
-        <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-          <div className="album-details">
-            <h5>ALBUM</h5>
-            <h1 id="album-title">{this.state.album.title}</h1>
-            <h5>ARTIST</h5>
-            <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}</div>
-          </div>
-        </section>
-        <table id="song-list">
-          <colgroup>
-            <col id="song-number-column" />
-            <col id="song-title-column" />
-            <col id="song-duration-column" />
-          </colgroup>
+render() {
+   return (
+     <section className="album">
+       <section id="album-info">
+         <img id="album-cover-art" src={this.state.album.albumCover} />
+         <div className="album-details">
+           <h1 id="album-title">{this.state.album.title}</h1>
+           <h2 className="artist">{this.state.album.artist}</h2>
+           <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
+         </div>
+       </section>
+       <table id="song-list">
+         <colgroup>
+           <col id="song-number-column" />
+           <col id="song-title-column" />
+           <col id="song-duration-column" />
+         </colgroup>
           <tbody>
             {this.state.album.songs.map( (song, index) =>
               <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
@@ -118,11 +133,19 @@ mouseLeave(e) {
                   </button>
                 </td>
                 <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
 
               </tr>
             )}
           </tbody>
         </table>
+        <PlayerBar
+        isPlaying={this.state.isPlaying}
+        currentSong={this.state.currentSong}
+        handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+        handlePrevClick={() => this.handlePrevClick()}
+        handleNextClick={() => this.handleNextClick()}
+        />
   </section>
    );
  }

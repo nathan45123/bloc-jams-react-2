a99b03b7c126df421a260b4a5244dea50141fa05
unknown <n.navarijo@gmail.com>
2019-03-19 18:14:50 -0500

    ISHOVERED

--- bloc/bloc-jams-react/src/components/Album.js
+++ bloc/bloc-jams-react/src/components/Album.js
@@ -4,7 +4,7 @@ import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     const album = albumData.find( album => {
       return album.slug === this.props.match.params.slug
@@ -13,32 +13,59 @@ class Album extends Component {
     this.state = {
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
       isPlaying: false,
-      duration: album.songs[0].duration
-
+      isHovered:false,
+      dynamicClass: 'song-number',
+      targetId: 0
     };
 
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
   }
 
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    };
+
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
+  }
+
   play() {
     this.audioElement.play();
     this.setState({
       isPlaying: true,
-      dynamicClass: 'icon-ion-ios-pause'
-    });
-  }
-
-
-  pause() {
-    this.audioElement.pause();
-    this.setState({
-      isPlaying: false,
       dynamicClass: 'icon ion-ios-play'
     });
   }
 
+  pause() {
+    this.audioElement.pause();
+    this.setState({
+       isPlaying: false,
+       dynamicClass: 'icon ion-ios-play'
+     });
+  }
 
   setSong(song) {
     this.audioElement.src = song.audioSrc;
@@ -50,105 +77,84 @@ class Album extends Component {
     if (this.state.isPlaying && isSameSong) {
       this.pause();
     } else {
-      if (!isSameSong) { this.setSong(song) }
+      if (!isSameSong) { this.setSong(song); }
       this.play();
     }
   }
 
   handlePrevClick() {
+       const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+       const newIndex = Math.max(0, currentIndex - 1);
+       const newSong = this.state.album.songs[newIndex];
+       this.setSong(newSong);
+       this.play();
+
+    }
+
+
+   handleNextClick() {
      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-     const newIndex = Math.max(0, currentIndex - 1);
+     const newIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
      const newSong = this.state.album.songs[newIndex];
      this.setSong(newSong);
      this.play();
-
-  }
-
-
- handleNextClick() {
-   const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-   const newIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
-   const newSong = this.state.album.songs[newIndex];
-   this.setSong(newSong);
-   this.play();
-  }
-
-
-  mouseEnter(e) {
-    console.log(e.target.id);
-    if (e.target !== this.state.currentSong && !this.state.isPlaying) {
-      this.setState({
-        dynamicClass: 'icon ion-ios-play',
-        targetId: e.target.id
-      });
     }
-  }
-
-
-mouseLeave(e) {
-  console.log(e.target.id);
-  if (!this.state.currentSong || !this.state.isPlaying) {
-    this.setState({
-      dynamicClass: 'song-number',
-      targetId: e.target.id
-    });
-  }
-  else if (this.state.currentSong) {
-    this.setState({
-      dynamicClass: 'icon ion-ios-pause',
-      targetId: e.target.id
-    });
-  }
-}
 
 
 
 
 
-render() {
-   return (
-     <section className="album">
-       <section id="album-info">
-         <img id="album-cover-art" src={this.state.album.albumCover} />
-         <div className="album-details">
-           <h1 id="album-title">{this.state.album.title}</h1>
-           <h2 className="artist">{this.state.album.artist}</h2>
-           <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
-         </div>
-       </section>
-       <table id="song-list">
-         <colgroup>
-           <col id="song-number-column" />
-           <col id="song-title-column" />
-           <col id="song-duration-column" />
-         </colgroup>
-          <tbody>
-            {this.state.album.songs.map( (song, index) =>
-              <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-                <td className="song-actions">
-                  <button>
-                    <span className={(this.state.isPlaying && (this.state.currentSong === song)) ?'hidden-number' : 'song-number'}>{index + 1}</span>
-                    <span className={(this.state.isPlaying && (this.state.currentSong === song)) ? 'icon ion-ios-pause' : ''}></span>
-                    <span className={(this.state.isPlaying && (this.state.currentSong === song)) ? '' : 'icon ion-ios-play'}></span>
-                  </button>
-                </td>
-                <td className="song-title">{song.title}</td>
-                <td className="song-duration">{song.duration}</td>
-
+   render() {
+    return (
+      <section className="album">
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+           <tbody className="list-group">
+            <tr>
+              <th>Track </th>
+              <th> Title</th>
+              <th>Duration</th>
               </tr>
-            )}
-          </tbody>
-        </table>
-        <PlayerBar
-        isPlaying={this.state.isPlaying}
-        currentSong={this.state.currentSong}
-        handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-        handlePrevClick={() => this.handlePrevClick()}
-        handleNextClick={() => this.handleNextClick()}
-        />
-  </section>
-   );
+             {this.state.album.songs.map( (song, index) =>
+               <tr className="song" key={index} onClick={() => this.handleSongClick(song)}
+                onMouseEnter={() => this.setState({ isHovered: index + 1})}
+                onMouseLeave={() => this.setState({ isHovered: false })}>
+                 <td className="song-actions">
+                 <button>
+                 {this.state.currentSong.title === song.title ?
+                 (<span className={this.state.isPlaying ? "ion-pause" : "ion-play"} />) :
+                 this.state.isHovered === index + 1 ? (<span className="ion-play" />) :
+                 (<span className="song-number">{index + 1}</span>)}
+               </button>
+                 </td>
+                 <td className="song-title">{song.title}</td>
+                 <td className="song-duration">{song.duration}</td>
+               </tr>
+             )}
+           </tbody>
+         </table>
+         <PlayerBar
+         isPlaying={this.state.isPlaying}
+         currentSong={this.state.currentSong}
+         handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+         handlePrevClick={() => this.handlePrevClick()}
+         handleNextClick={() => this.handleNextClick()}
+         />
+   </section>
+    );
+  }
  }
-}
 
-export default Album;
+ export default Album;
